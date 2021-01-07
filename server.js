0e67ccad2ca1ce57b4081eec7b6f994511c1f70a
DESKTOP-NHGUI1P\myjob <myjob63@mail.ru>
2021-01-07 21:01:59 +0300

    srv file moved to the root dir

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = JSONserver.create();
 const router = JSONserver.router("./public/db.json");
 const middlewares = JSONserver.defaults({ static: "./build" });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 server.use(middlewares);
 server.use(router);
 server.listen(PORT, () => {
